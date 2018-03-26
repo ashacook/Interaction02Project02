c901b8f4cb1f219736284ed48fea08e31ce1bbba
asha cook <acook2@collegeforcreativestudies.edu>
2018-03-26 15:26:33 -0400

    code

---
+++ p2_beta/scripts/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+$('.page').click(function() {
+	$(this).fadeOut("slow");
+});
+
+
+
+/*
+var currentLetter = 02;
+
+$('letter02').click(function() {
+ $('box02-' + currentLetter).fadeOut(200);
+ currentLetter = currentLetter + 1;
+ */
+});
